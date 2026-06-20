faab3a162189d2d870b01092941d4abb7eea88bc
Rino Godson <rinogodson@gmail.com>
2026-06-20 17:53:29 +0530

    basic boilerplate lua love2d thing!

---
+++ main.lua
@@ -0,0 +1,10 @@
+function love.load()
+	love.window.setTitle("spintral")
+	love.window.setMode(960, 540)
+end
+
+function love.update(dt) end
+
+function love.draw() end
+
+function love.mousepressed() end
